bf5f19ff72b201ea47af72391b601493ca9c095d
Oliver Newland <oliver.newland@crowdflower.com>
2018-01-30 13:50:18 -0800

    send/receive thrift messages over rabbitMQ in ruby

--- src/ruby/mq_producer.rb
+++ src/ruby/mq_producer.rb
@@ -1,7 +1,17 @@
 require 'bunny'
+require 'faker'
 
 $:.push('thrift-classes')
 
 require 'first_constants'
 
-puts IntAndString.new
+$conn = Bunny.new("amqp://guest:guest@localhost:5672")
+$conn.start
+$ch = $conn.channel
+$queue = $ch.queue("abcdefghi", auto_delete: true)
+$serializer = Thrift::Serializer.new
+
+1000.times do |i|
+  struct = IntAndString.new(name: Faker::Name.name, count: i)
+  $queue.publish($serializer.serialize(struct))
+end
