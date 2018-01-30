f89dc22cfe5016ed0df48a5a460a4acfb179adf7
Oliver Newland <oliver.newland@crowdflower.com>
2018-01-30 15:44:22 -0800

    add ruby consumer

---
+++ src/ruby/mq_consumer.rb
@@ -0,0 +1,18 @@
+require 'bunny'
+require 'faker'
+
+$:.push('thrift-classes')
+
+require 'first_constants'
+
+$conn = Bunny.new("amqp://guest:guest@localhost:5672")
+$conn.start
+$ch = $conn.channel
+$queue = $ch.queue("abcdefghi", auto_delete: true)
+$deserializer = Thrift::Deserializer.new
+
+$queue.subscribe(block: true) do |delivery_info, properties, payload|
+  obj = IntAndString.new
+  $deserializer.deserialize(obj, payload)
+  puts "#{obj.name}, #{obj.count}"
+end
