50fc6491cd6122a1488743c4b34d126dcd6da4f5
Oliver Newland <oliver.newland@crowdflower.com>
2018-01-30 16:09:47 -0800

    add rabbitmq message production for nodejs

--- src/js/mq_producer.js
+++ src/js/mq_producer.js
@@ -2,30 +2,42 @@
 
 var thrift = require('thrift');
 var ttypes = require('./thrift-classes/first_types');
+var amqp = require('amqplib');
 
-let x = new ttypes.IntAndString();
-x.name = 'Oliver'
-x.count = 100
-console.log(x);
-
-let writerTransport = new thrift.TBufferedTransport(
-    new Buffer(""),
-    msg => console.log(readBuf(msg))
-);
-
-let writerProto = new thrift.TBinaryProtocol(writerTransport);
+amqp.connect("amqp://guest:guest@localhost:5672");
 
 function readBuf(msg) {
     let y = new ttypes.IntAndString();
     let readerTransport = new thrift.TFramedTransport(
         new Buffer(msg)
-    )
+    );
     let readerProto = new thrift.TBinaryProtocol(readerTransport);
-
-    readerTransport.write(msg);
     y.read(readerProto);
     return y;
 }
 
-x.write(writerProto);
-writerProto.flush();
+async function generateMessages(messageCount)  {
+    let conn = await amqp.connect('amqp://localhost');
+    let ch = await conn.createChannel();
+    let q = 'abcdefghi';
+    let ok = await ch.assertQueue(q, {durable: false, autoDelete: true});
+
+    for(let i = 0; i < messageCount; i++) {
+        let x = new ttypes.IntAndString();
+        x.name = 'Oliver';
+        x.count = i;
+
+        let writerTransport = new thrift.TBufferedTransport(
+            new Buffer(""),
+            msg => ch.sendToQueue(q, msg)
+        );
+        let writerProto = new thrift.TBinaryProtocol(writerTransport);
+        x.write(writerProto);
+        writerProto.flush();
+    }
+
+    console.log('got here');
+    setTimeout(function() { conn.close(); process.exit(0) }, 500);
+}
+
+generateMessages(100);
